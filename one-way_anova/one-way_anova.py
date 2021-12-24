3e31733ca319295bc5019f649fa86db30b86233c
TyKo0707 <super.family.timut@gmail.com>
2021-12-25 01:10:07 +0200

    One-way ANOVA update

--- one-way_anova/one-way_anova.py
+++ one-way_anova/one-way_anova.py
@@ -11,15 +11,16 @@ data.boxplot()
 print('Null hypothesis: ', '='.join(data))
 print('Alternative hypothesis: ', f'!({"=".join(data)})')
 # Total average
-grand_mean = data.values.flatten().mean()
+grand_mean = data.values.flatten().mean()  # .flatten() - Return a copy of the array collapsed into one dimension
 # deviation of the group average from the total average
 ssb = sum(data[group].size * (group_mean - grand_mean) ** 2 for group, group_mean in data.mean().items())
 # deviations of values within the group from the average group
 ssw = sum(sum((x - group_mean) ** 2 for x in data[group]) for group, group_mean in data.mean().items())
 
-groups = data.shape[1]
-dfb = groups - 1
-dfw = data.size - groups
+groups = data.shape  # .shape() - Return a tuple representing the dimensionality of the DataFrame
+print(groups)  # (3, 3)
+dfb = groups[1] - 1
+dfw = data.size - groups[1]
 # SSB
 mssb = ssb / dfb
 # SSW
