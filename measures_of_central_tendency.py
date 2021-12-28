0aca1dcd0274db4088a9947a6f9b75000dc2e4cd
TyKo0707 <super.family.timut@gmail.com>
2021-12-29 01:03:29 +0200

    Tukey correction (+rename bonferroni_correction)

--- measures_of_central_tendency.py
+++ measures_of_central_tendency.py
@@ -3,23 +3,21 @@ from scipy import stats
 import pandas as pd
 
 sample_np = np.array([185, 175, 170, 169, 171, 175, 157, 172, 170, 172, 167, 173, 168, 167, 166,
-                   167, 169, 172, 177, 178, 165, 161, 179, 159, 164, 178, 172, 170, 173, 171])
+                      167, 169, 172, 177, 178, 165, 161, 179, 159, 164, 178, 172, 170, 173, 171])
 
 sample_pd = pd.Series([185, 175, 170, 169, 171, 175, 157, 172, 170, 172, 167, 173, 168, 167, 166,
-              167, 169, 172, 177, 178, 165, 161, 179, 159, 164, 178, 172, 170, 173, 171])
+                       167, 169, 172, 177, 178, 165, 161, 179, 159, 164, 178, 172, 170, 173, 171])
 
 print('sorted array:', sorted(sample_np), sorted(sample_pd))
 '''[157, 159, 161, 164, 165, 166, 167, 167, 167, 168, 169, 169, 170, 170, 170, 
     171, 171, 172, 172, 172, 172, 173, 173, 175, 175, 177, 178, 178, 179, 185]'''
-
 
 # SCIPY and NUMPY
 print('mode scipy:', stats.mode(sample_np))  # mode: ModeResult(mode=array([172]), count=array([4]))
 print('median numpy:', np.median(sample_np))  # median: 170.5
 print('mean numpy:', np.mean(sample_np))  # mean: 170.4
 
-
 # PANDAS
-print('mode pandas:', sample_pd.mode()) # mode pandas: 172
-print('median pandas:', sample_pd.median()) # median pandas: 170.5
-print('mean pandas:', sample_pd.mean()) # mean pandas: 170.4
+print('mode pandas:', sample_pd.mode())  # mode pandas: 172
+print('median pandas:', sample_pd.median())  # median pandas: 170.5
+print('mean pandas:', sample_pd.mean())  # mean pandas: 170.4
---
+++ multiple_comparisons_anova/tukey_correction.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+np.random.seed(123)
+a = []
+b = []
+c = []
+d = []
+
+df = pd.DataFrame({'RT': np.random.randn(100), 'Cond': np.random.choice(['A', 'B', 'C', 'D'], 100)})
+
+for num, gr in df.Cond.items():
+    match gr:
+        case 'A':
+            a.append(df.RT[num])
+        case 'B':
+            b.append(df.RT[num])
+        case 'C':
+            c.append(df.RT[num])
+        case 'D':
+            d.append(df.RT[num])
+
+print(f'A: \n\tMean: {pd.Series(data=a).mean()}\n\tMedian: {pd.Series(data=a).median()}'
+      f'\n\tMin: {min(pd.Series(data=a))}\n\tMax: {max(pd.Series(data=a))}\n'
+      f'B: \n\tMean: {pd.Series(data=b).mean()}\n\tMedian: {pd.Series(data=b).median()}'
+      f'\n\tMin: {min(pd.Series(data=b))}\n\tMax: {max(pd.Series(data=b))}\n'
+      f'C: \n\tMean: {pd.Series(data=c).mean()}\n\tMedian: {pd.Series(data=c).median()}'
+      f'\n\tMin: {min(pd.Series(data=c))}\n\tMax: {max(pd.Series(data=c))}\n'
+      f'D: \n\tMean: {pd.Series(data=d).mean()}\n\tMedian: {pd.Series(data=d).median()}'
+      f'\n\tMin: {min(pd.Series(data=d))}\n\tMax: {max(pd.Series(data=d))}\n')
+
+hs_res = pairwise_tukeyhsd(df["RT"], df['Cond'])
+print(hs_res)
